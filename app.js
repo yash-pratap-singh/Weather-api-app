4f527ee05cc726ca63302c46beb4f24aea6a0ce0
Yash Pratap Singh <yashrana1311@gmail.com>
2022-06-10 17:58:46 +0530

    update 2.0

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express =require('express');
 const app=express();
 const path=require('path');
 const routes=require('./route/routes');
+require('dotenv').config();
 
 const PORT=process.env.PORT||3500;
 app.use(express.static('./views'));
